335e2fc0021bbd854693c818a5ab6cc6221c1944
weeger <romain.weeger@gmail.com>
2023-03-01 17:31:49 +0100

    #68 system/ip fix

--- bash/system/ip.sh
+++ bash/system/ip.sh
@@ -6,16 +6,5 @@ systemIpArgs() {
 }
 
 systemIp() {
-  if [ "$(wex-exec docker/isToolBox)" = true ]; then
-    wex-exec docker/ip
-    return
-  fi
-  # Enforce language for parsing.
-  export LC_ALL=C
-  # May have several IP's
-  IPS=($(ip route get 198.51.100.1 | sed -n 's/.* src \([^[:space:]]*\) .*/\1/p'))
-  # Revert to default language.
-  unset LC_ALL
-  # Take the last one
-  echo "${IPS[-1]}"
+  hostname -I | awk '{print $1}'
 }
